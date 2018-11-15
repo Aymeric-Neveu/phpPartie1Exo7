b4169c6ca8d30763253f3d98a1f7c1dc2a214bb0
Aymeric Neveu <emploi.neveu@gmail.com>
2018-11-15 11:40:44 +0100

    exo7

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP</title>
+  </head>
+  <body>
+    <?php
+    $name = "Rico";
+    $age = "18 ans";
+    $passion = "foot";
+    echo "Bonjour $name , tu as $age et tu aimes le $passion";
+    ?>
+  </body>
